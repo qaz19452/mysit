b5888bab04278ee13b593ad9740adfabe852f273
qaz19452 <45327502+qaz19452@users.noreply.github.com>
2020-07-19 13:23:28 +0800

    使用通用模板

--- polls/views.py
+++ polls/views.py
@@ -3,34 +3,24 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 from django.template import loader
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request,'polls/detail.html', {"question":question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
-
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
